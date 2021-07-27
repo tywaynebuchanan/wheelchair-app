192de9f0a9c945edcfaddfa7f134c746ee4997b4
Tywayne <tywaynebuchanan@gmail.com>
2021-07-27 11:24:07 -0500

    made changes to dbconn.php for connection to herok

--- dbconn.php
+++ dbconn.php
@@ -1,12 +1,27 @@
   
-<?php
+<!-- <?php
 $conn_error = "Could not connect";
-$db_host = "mysql-41134-0.cloudclusters.net";
-$db_user ="admin";
-$db_password ="uwzBiAbY";
-$port = "19703";
-$mysql_db ="wms";
-$conn = mysqli_connect($db_host,$db_user,$db_password,$port) or die ('The username or password is incorrect');
+$db_host = 'us-cdbr-east-04.cleardb.com';
+$db_user ='b2d5ec8d1a0f17';
+$db_password ='330c4802';
+// $port = '19703';
+$mysql_db ='heroku_46bb92508100522';
+$conn = mysqli_connect($db_host,$db_user,$db_password) or die ('The username or password is incorrect');
 mysqli_select_db($conn,$mysql_db) or die('Not such database present');
 
-?>
+?> -->
+
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+
