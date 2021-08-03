c2b543d3f5ad0172e6fafa3d35e57a2764a8d4b4
Tywayne <tywaynebuchanan@gmail.com>
2021-08-03 08:28:05 -0500

    changes to style of mfh.php

--- dbconn.php
+++ dbconn.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
@@ -9,4 +9,5 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-?>
+?> -->
+
--- mfh/mfh.php
+++ mfh/mfh.php
@@ -15,14 +15,33 @@
   <title>WheelChair Management System</title>
 </head>
 <body>
-
+<!-- 
 <nav>
         <div class="nav-container">
            <div class="wrapper">
               <a href="../homepage.php"><img src="../images/logo.png" alt="Mustard Seed Logo"></a>
            </div>
+           <div class="navbar">
+              <a href="../logout.php">Logout</a>
+           </div>
      </div>
-     </nav>
+     </nav> -->
+
+   
+   <header >
+    
+      <img src="../images/logo.png" alt="Mustard Seed Logo" class="logo">
+      <nav>
+         <ul class="nav-area">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Homes</a></li>
+            <li><a href="#">View Residents</a></li>
+         </ul>
+      </nav>
+      <a href="../logout.php" class="btn-area">Logout</a>
+     
+      
+   </header>
 
 <section>
       <div class="hero-container">
