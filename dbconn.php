38c6cf521336833a3f14dee012c6fbedcaa5c6b5
Tywayne <tywaynebuchanan@gmail.com>
2021-08-02 20:57:17 -0500

    made changes to dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -11,13 +11,3 @@ $query_builder = TRUE;
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
   
-
-<!-- <?php
-$conn_error = "Could not connect";
-$db_host = 'localhost';
-$db_user ='root';
-$db_password ='';
-$mysql_db ='wheelchairapp';
-$conn = mysqli_connect($db_host,$db_user,$db_password) or die ('The username or password is incorrect');
-mysqli_select_db($conn,$mysql_db) or die('Not such database present');
-?>  -->
