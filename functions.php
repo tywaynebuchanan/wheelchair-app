4905538d78b688ecfbc8cb40c72af640d7f61e48
Tywayne <tywaynebuchanan@gmail.com>
2021-08-02 21:30:02 -0500

    made changes to functions.php

--- functions.php
+++ functions.php
@@ -8,14 +8,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST" & !empty($_POST))
   $username = mysqli_escape_string($conn,$_POST['username']);
   $password = md5($_POST['password']);
 
+  $username = stripslashes($myusername);
+  $password = stripslashes($mypassword);
+
   $query = mysqli_query($conn,"SELECT * from tblusers where username = '$username' AND password = '$password'");
 
   $row = mysqli_fetch_array($query);
   $count = mysqli_num_rows($query);
 
+
+  $role = $row['role'];
+    if($role =='Administrator'){
+        $link = 'homepage.php';
+        }
+       elseif($role =='Viewer')
+       {
+        $link = 'mfh.php';
+        }
+
   if($count == 1) {
-       
-    header("location: homepage.php");
+    
+    $_session["username"] = $username;
+    $_session["password"] = $password;
+$_session["role"] = $row['role'];
+header("Location: ".$link."");
+  
+
+
+    // header("location: homepage.php");
  }else {
     echo "Your Login Name or Password is invalid";
  }
